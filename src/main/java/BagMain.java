4d68c9aa6aa096765d53e18e999c2f6358198335
Sarah <lemiex@outlook.com>
2022-10-09 14:54:49 -0400

    fifth attempt

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -18,13 +18,13 @@ class BagMain {
         for(Bag bag : bags){
             if (bag instanceof HandBag){
                 ((HandBag) bag).enhance();
-                if(double_enhance_handbags){
+                if(double_enhance_handbags == true){
                     ((HandBag) bag).enhance();
                 }
             }
             else if (bag instanceof CrossbodyBag) {
                 ((CrossbodyBag) bag).enhance();
-                if (double_enhance_handbags) {
+                if (double_enhance_handbags == true) {
                     ((CrossbodyBag) bag).enhance();
                 }
             }
